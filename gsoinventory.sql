1531333722875f6d4eb1187b1d57c4f78a88e00c
Leymar2020 <108674355+Leymar2020@users.noreply.github.com>
2023-07-21 20:58:34 +0800

    only one table "items"

--- gsoinventory.sql
+++ gsoinventory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 19, 2023 at 03:25 PM
+-- Generation Time: Jul 21, 2023 at 02:46 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
