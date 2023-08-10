50e917df85d2c14286cf224d6ea9190baa2ece62
Joyce Bravo <cjbravo28@gmail.com>
2023-08-11 00:30:08 +0800

    manage approvals

--- index_user.php
+++ index_user.php
@@ -266,8 +266,8 @@
                                         $tquan = $i_quantity - $request_quantity;
 
                                         mysqli_query($db,"UPDATE items SET quantity='$tquan' WHERE id='$item_id'");
-                                        mysqli_query($db, "INSERT INTO requests (item_id,requester,item_name,dep_name,property_code,purpose,end_user,description,r_status,date,date_needed)
-                             VALUES ('$item_id','$requester','$item_name','$dep_name','$property_code','$purpose','$end_user','$description','$r_status','$date','$dateN')");
+                                        mysqli_query($db, "INSERT INTO requests (item_id,user_id,requester,item_name,dep_name,property_code,purpose,end_user,description,r_status,date,date_needed)
+                             VALUES ('$item_id','$user','$requester','$item_name','$dep_name','$property_code','$purpose','$end_user','$description','$r_status','$date','$dateN')");
 
                              ?>
                                 <script>
